37ec5734b4bb2b2a249d37ecc9e4960de68d15f5
ToastCoder <vicky.pcbasic@gmail.com>
2020-10-04 20:44:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,33 @@ import win10toast
 import time
 from tkinter import *
 
+message = 0
+deadline = 0
+
+def makeVariables():
+    global message
+    message = val1.get()
+    global deadline
+    deadline = val2.get()
+    print(message,deadline)
+    deadline = int(deadline) * 60
+    time.sleep(deadline)
+    notify = win10toast.ToastNotifier()
+    notify.show_toast("Reminder",message)
+root = Tk()
+root.title("Reminder")
+txt1 = Label(root,text = "Create a simple reminder\nKeep this application open inorder not to terminate the app.").pack()
+txt2 = Label(root,text = "Remind about?").pack()
+val1 = Entry(root)
+val1.pack()
+txt3 = Label(root,text = "In how many minutes?").pack()
+val2 = Entry(root)
+val2.pack()
+key = Button(root,text = "Set Reminder",width = 20,command = makeVariables)
+key.pack()
+
+root.mainloop()
+'''
 #Window Class
 class Window:
     def __init__(self,win):
@@ -46,3 +73,4 @@ time.sleep(deadline)
 notify = win10toast.ToastNotifier()
 notify.show_toast("Reminder",command)
 root.mainloop()
+'''
